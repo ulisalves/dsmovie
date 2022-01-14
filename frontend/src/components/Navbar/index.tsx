a9a48797a0442cc90ad60759039b6e5743ea4951
Ulisses Alves <ulisalves@gmail.com>
2022-01-13 21:34:07 -0300

    Navbar

---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,20 @@
+import { ReactComponent as GithubIcon } from 'assets/img/github.svg';
+import './styles.css'
+function Navbar() {
+    return (
+        <header>
+            <nav className="container">
+                <div className="dsmovie-nav-content">
+                    <h1>DSMovie</h1>
+                    <a href="https://github.com/devsuperior"></a>
+                    <div className="dsmovie-contact-container">
+                        <GithubIcon />
+                        <p className="dsmovie-contact-link">/devsuperior</p>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    );
+}
+
+export default Navbar;
